05d459d6c672dd3256d9296b4614378d6e63b332
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-06-05 19:20:35 +0300

    feat: validators added to register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { validateInput } from '../../validators/credsValidator';
 
 @Component({
   selector: 'app-register',
@@ -15,10 +16,33 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      login: ['', Validators.required],
-      password: ['', Validators.required],
+      name: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(100),
+        validateInput(/[^a-zA-Z ]/gi),
+      ]],
+      email: ['', [ // double check this regex
+        Validators.required,
+        validateInput(/^\S+@\S+$/),
+      ]],
+      login: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(100),
+        validateInput(/[^a-zA-Z0-9]/gi),
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(100),
+        validateInput(/[^a-zA-Z0-9_]/gi),
+      ]],
     });
+  }
+
+  submitForm() {
+    console.log(this.registerForm.value);
   }
 
 }
